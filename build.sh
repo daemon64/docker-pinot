a10e9d5f95c5388480af3350b9636381108b447c
Jean-Francois Im <jeanfrancois.im@gmail.com>
2017-12-18 14:34:33 -0800

    Parameterize Pinot version used in Docker builds

--- build.sh
+++ build.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 declare -a components=("controller" "server" "broker" "quickstart-offline" "quickstart-realtime")
 user="jfim"
+version=$1
 
-echo "Building base Pinot image $user/pinot"
-docker build -t ${user}/pinot:latest -f Dockerfile-pinot .
+echo "Building base Pinot image $user/pinot with version $version"
+docker build -t ${user}/pinot:$version -f Dockerfile-pinot --build-arg pinotVersion=$version .
+docker tag ${user}/pinot:$version ${user}/pinot:latest
 
 for component in "${components[@]}"
 do
   echo "Building image $user/pinot-$component"
-  docker build -t ${user}/pinot-${component}:latest -f Dockerfile-${component} .
+  docker build -t ${user}/pinot-${component}:$version -f Dockerfile-${component} --build-arg pinotVersion=$version .
 done
 
 echo "Pushing base Pinot image $user/pinot"
+docker push ${user}/pinot:$version
 docker push ${user}/pinot:latest
 
 for component in "${components[@]}"
 do
   echo "Pushing image $user/pinot-$component"
+  docker tag ${user}/pinot-${component}:$version ${user}/pinot-${component}:latest
+  docker push ${user}/pinot-${component}:$version
   docker push ${user}/pinot-${component}:latest
 done
